e0e3c6b561ef90a431593cc0e7b69af00f663afa
leander-dsouza <leanderdsouza1234@gmail.com>
2020-05-04 16:24:07 +0530

    .

---
+++ LSTM_generator.py
@@ -0,0 +1,85 @@
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout, Bidirectional
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import regularizers
+import tensorflow.keras.utils as ku
+import numpy as np
+from tensorflow.keras.models import model_from_yaml
+import os
+import tensorflow as tf
+
+tokenizer = Tokenizer()
+data = open('Arctic_Monkeys.txt').read()
+
+corpus = data.lower().split("\n")
+tokenizer.fit_on_texts(corpus)
+total_words = len(tokenizer.word_index) + 1
+
+# create input sequences using list of tokens
+input_sequences = []
+for line in corpus:
+	token_list = tokenizer.texts_to_sequences([line])[0]
+	for i in range(1, len(token_list)):
+		n_gram_sequence = token_list[:i+1]
+		input_sequences.append(n_gram_sequence)
+
+
+# pad sequences
+max_sequence_len = max([len(x) for x in input_sequences])
+input_sequences = np.array(pad_sequences(input_sequences, maxlen=max_sequence_len, padding='pre'))
+
+# create predictors and label
+predictors, label = input_sequences[:,:-1],input_sequences[:,-1]
+
+label = ku.to_categorical(label, num_classes=total_words)
+
+model = Sequential()
+model.add(Embedding(total_words, 100, input_length=max_sequence_len-1))
+model.add(Bidirectional(LSTM(150, return_sequences = True)))
+model.add(Dropout(0.2))
+model.add(LSTM(100))
+model.add(Dense(total_words/2, activation='relu', kernel_regularizer=regularizers.l2(0.01)))
+model.add(Dense(total_words, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model.summary())
+
+checkpoint_path = "training_1/cp.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# Create a callback that saves the model's weights every 5 epochs
+cp_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_path,
+    verbose=1,
+    save_weights_only=True,
+    period=5)
+
+
+model.load_weights(checkpoint_path)
+
+history = model.fit(predictors, label, epochs=500, verbose=1,callbacks=[cp_callback])
+
+model.save("arctic_monkeys_model.h5")
+print("Saved model to disk")
+
+import matplotlib.pyplot as plt
+acc = history.history['accuracy']
+loss = history.history['loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'b', label='Training accuracy')
+plt.title('Training accuracy')
+
+plt.figure()
+
+plt.plot(epochs, loss, 'b', label='Training Loss')
+plt.title('Training loss')
+plt.legend()
+
+plt.show()
+
+
+
+
---
+++ poetry_generator.py
@@ -0,0 +1,48 @@
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import numpy as np
+from tensorflow.keras.models import load_model
+
+tokenizer = Tokenizer()
+data = open('Arctic_Monkeys.txt').read()
+
+corpus = data.lower().split("\n")
+tokenizer.fit_on_texts(corpus)
+total_words = len(tokenizer.word_index) + 1
+
+# create input sequences using list of tokens
+input_sequences = []
+for line in corpus:
+	token_list = tokenizer.texts_to_sequences([line])[0]
+	for i in range(1, len(token_list)):
+		n_gram_sequence = token_list[:i+1]
+		input_sequences.append(n_gram_sequence)
+
+
+# pad sequences
+max_sequence_len = max([len(x) for x in input_sequences])
+input_sequences = np.array(pad_sequences(input_sequences, maxlen=max_sequence_len, padding='pre'))
+
+
+
+
+
+
+model = load_model('arctic_monkeys_model.h5')
+
+
+
+seed_text = "Let us"
+next_words = 5
+
+for _ in range(next_words):
+	token_list = tokenizer.texts_to_sequences([seed_text])[0]
+	token_list = pad_sequences([token_list], maxlen=max_sequence_len - 1, padding='pre')
+	predicted = model.predict_classes(token_list, verbose=0)
+	output_word = ""
+	for word, index in tokenizer.word_index.items():
+		if index == predicted:
+			output_word = word
+			break
+	seed_text += " " + output_word
+print(seed_text)
